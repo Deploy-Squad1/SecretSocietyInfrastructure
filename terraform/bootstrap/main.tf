835fe72d201b1c3ad246c1c60000fd9bf3ce80cd
veronikas <syvaveronika@gmail.com>
2026-03-15 23:54:14 +0200

    chore(infra):DS-37 add TF backend bootstrap config

---
+++ terraform/bootstrap/main.tf
@@ -0,0 +1,26 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "secret-society-tf-state-deploysquad"
+
+  tags = {
+    Name    = "terraform-state"
+    Project = "secret-society"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}

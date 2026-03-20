0eaf96e4a9366a5f59b0ad870cf40169075c8626
veronikas <syvaveronika@gmail.com>
2026-03-20 21:35:27 +0200

    chore(terraform): DS-61 add reusable EKS module

---
+++ terraform/modules/eks/outputs.tf
@@ -0,0 +1,19 @@
+output "cluster_name" {
+  description = "The name of the EKS cluster"
+  value       = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint for the Kubernetes API server"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "ID of the cluster security group"
+  value       = module.eks.cluster_security_group_id
+}
+
+output "node_security_group_id" {
+  description = "ID of the node shared security group"
+  value       = module.eks.node_security_group_id
+}

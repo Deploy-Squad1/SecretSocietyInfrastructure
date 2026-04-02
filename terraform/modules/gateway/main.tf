688aa7a4e2b7cf6a7a72ef4ee7262fb1f8a4652d
veronikas <syvaveronika@gmail.com>
2026-04-02 20:45:04 +0300

    fix(gateway): DS-72 adjust nginx gateway deployment

--- terraform/modules/gateway/main.tf
+++ terraform/modules/gateway/main.tf
@@ -1,10 +1,19 @@
+# Install Gateway API CRDs
+resource "null_resource" "gateway_api_crds" {
+  provisioner "local-exec" {
+    command = "kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.1.0/standard-install.yaml"
+  }
+}
+
 # Install NGINX Gateway Fabric controller
 resource "helm_release" "nginx_gateway" {
   name             = "nginx-gateway"
-  repository       = "https://helm.nginx.com/stable"
+  repository       = "oci://ghcr.io/nginxinc/charts"
   chart            = "nginx-gateway-fabric"
   namespace        = "nginx-gateway"
   create_namespace = true
+
+  depends_on = [null_resource.gateway_api_crds]
 
   values = [
     yamlencode({
@@ -16,18 +25,6 @@ resource "helm_release" "nginx_gateway" {
         image = {
           repository = var.gateway_image_repository
           tag        = var.gateway_image_tag
-        }
-
-        # Temporary: disable TLS so controller listens on HTTP
-        tls = {
-          enable = false
-        }
-      }
-
-      nginx = {
-        image = {
-          repository = var.nginx_image_repository
-          tag        = var.nginx_image_tag
         }
       }
     })
--- terraform/modules/gateway/variables.tf
+++ terraform/modules/gateway/variables.tf
@@ -7,14 +7,3 @@ variable "gateway_image_tag" {
   description = "Image tag"
   type        = string
 }
-
-variable "nginx_image_repository" {
-  description = "ECR repository for nginx dataplane"
-  type        = string
-}
-
-variable "nginx_image_tag" {
-  description = "Image tag for nginx dataplane"
-  type        = string
-  default     = "latest"
-}

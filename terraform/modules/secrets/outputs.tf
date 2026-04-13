daaf6db6145ad711f635779a0572f49500bcebc3
veronikas <syvaveronika@gmail.com>
2026-04-13 11:22:00 +0300

    fix(secrets): DS-78 stabilize DB credentials handling

--- terraform/modules/secrets/outputs.tf
+++ terraform/modules/secrets/outputs.tf
@@ -2,3 +2,14 @@ output "map_service_secret_arn" {
   description = "ARN of existing map-service secret"
   value       = data.aws_secretsmanager_secret.map_service.arn
 }
+
+output "db_password" {
+  description = "Database password"
+  value       = random_password.db.result
+  sensitive   = true
+}
+
+output "db_secret_arn" {
+  description = "ARN of the database secret"
+  value       = aws_secretsmanager_secret.db.arn
+}
